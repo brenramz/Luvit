ab637f0cf2be1ae1fc53c2ab47f95247ff917e76
brenramz <breen.ramz@gmail.com>
2020-07-12 15:09:08 -0500

    Adding db configuration

--- luvitdjango/settings.py
+++ luvitdjango/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'l3&_zj2wi1#a^ziv_^i@0ck+e1n8d*2@y-uzr#_6oox1$ch0lz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['luvit-env.eba-jbjf4g2s.us-east-1.elasticbeanstalk.com']
-
+ALLOWED_HOSTS = ['http://luvit-env.eba-jbjf4g2s.us-east-1.elasticbeanstalk.com']
 
 # Application definition
 
@@ -72,25 +71,16 @@ WSGI_APPLICATION = 'luvitdjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-IF 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+}
 
 
 # Password validation
