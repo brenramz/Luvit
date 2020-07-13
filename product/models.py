81364c3157263bcc75620cd09b1ac3b9bad97d06
brenramz <breen.ramz@gmail.com>
2020-07-12 19:45:25 -0500

    Fix product model
    Fix name min lenght.
    Allow discount value field be nullable.

--- product/models.py
+++ product/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator
 from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Product(models.Model):
     name = models.CharField(
-        'name', min_length=3, max_length=55)
+        'name', max_length=55,
+        validators=[MinLengthValidator(3)])
     value = models.DecimalField(
         'value', decimal_places=1,
         validators=[MinValueValidator(0.0), MaxValueValidator(99999.9)])
     discount_value = models.DecimalField(
-        'discount value', decimal_places=1,
+        'discount value', decimal_places=1, null=True, blank=True,
         validators=[MinValueValidator(0.0), MaxValueValidator(99999.9)])
     stock = models.IntegerField(
         'stock', validators=[MinValueValidator(-1)])
