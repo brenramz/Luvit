5af1aa4e6d19eabda0c837d3b86e9adaa1be0f18
brenramz <breen.ramz@gmail.com>
2020-07-12 23:29:58 -0500

    Override form error messages

--- api/product/forms.py
+++ api/product/forms.py
@@ -6,6 +6,23 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+        error_messages = {
+            'name': {
+                'min_length': 'Invalid product name',
+                'max_length': 'Invalid product name'
+            },
+            'value': {
+                'min_value': 'Invalid value',
+                'max_value': 'Invalid value'
+            },
+            'discount_value': {
+                'min_value': 'Invalid discount value',
+                'max_value': 'Invalid discount value'
+            },
+            'stock': {
+                'min_value': 'Invalid stock value'
+            }
+        }
 
     def save(self, commit=True):
         instance = super(ProductForm, self).save(commit=False)
