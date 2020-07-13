c9c4d4a3e9007f8352fd3d78e8bb153f972452cf
brenramz <breen.ramz@gmail.com>
2020-07-12 19:38:07 -0500

    Add product app

---
+++ product/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible
+class Product(models.Model):
+    name = models.CharField(
+        'name', min_length=3, max_length=55)
+    value = models.DecimalField(
+        'value', decimal_places=1,
+        validators=[MinValueValidator(0.0), MaxValueValidator(99999.9)])
+    discount_value = models.DecimalField(
+        'discount value', decimal_places=1,
+        validators=[MinValueValidator(0.0), MaxValueValidator(99999.9)]))
+    stock = models.IntegerField(
+        'stock', validators=[MinValueValidator(-1)])
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
