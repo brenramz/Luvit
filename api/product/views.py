d2f5c3cbadb8d75711e4ff32503b086f08a423db
brenramz <breen.ramz@gmail.com>
2020-07-12 23:05:47 -0500

    Update views.py

--- api/product/views.py
+++ api/product/views.py
@@ -42,34 +42,32 @@ def product_create(request):
         products = []
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
-        print('request post: ', body)
-        return HttpResponse(body['products'], status=200, content_type="application/json")
-        # for product in body['products']:
-        #     product_form = forms.ProductForm(product, instance=Product())
-        #     if product_form.is_valid():
-        #         product = product_form.save()
-        #         products.append(product)
-        #     else:
-        #         all_valid = False
-        #         count_product_errors += 1
-        #         errors = []
-        #         for key,value in reschedule_form.errors.items():
-        #             error_msg +="{}: {}".format(key, ','.join(value))
-        #             errors.append(error_msg)
-        #         products_report.append({
-        #             'product_id': str(product.id),
-        #             'errors': errors
-        #         })
-        # # Add products in a bulk creation
-        # if not all_valid:
-        #     response = json.dumps({
-        #         'status': 'ERROR',
-        #         'products_report': products_report,
-        #         'number_of_products_unable_to_parse': count_product_errors
-        #     })
-        #     return HttpResponse(response, status=422, content_type="application/json")
-        # Product.objects.bulk_create(products)
-        # response = json.dumps({
-        #     'status': 'OK'
-        # })
-        # return HttpResponse(response, status=200, content_type="application/json")
+        for product in body['products']:
+            product_form = forms.ProductForm(product, instance=Product())
+            if product_form.is_valid():
+                product = product_form.save()
+                products.append(product)
+            else:
+                all_valid = False
+                count_product_errors += 1
+                errors = []
+                for key,value in reschedule_form.errors.items():
+                    error_msg +="{}: {}".format(key, ','.join(value))
+                    errors.append(error_msg)
+                products_report.append({
+                    'product_id': str(product.id),
+                    'errors': errors
+                })
+        # Add products in a bulk creation
+        if not all_valid:
+            response = json.dumps({
+                'status': 'ERROR',
+                'products_report': products_report,
+                'number_of_products_unable_to_parse': count_product_errors
+            })
+            return HttpResponse(response, status=422, content_type="application/json")
+        Product.objects.bulk_create(products)
+        response = json.dumps({
+            'status': 'OK'
+        })
+        return HttpResponse(response, status=200, content_type="application/json")
