6f5c2d848c8ce3a5fd789524d93ceac0ea96c23f
brenramz <breen.ramz@gmail.com>
2020-07-12 21:29:59 -0500

    Fix product creation view

--- api/product/views.py
+++ api/product/views.py
@@ -10,36 +10,37 @@ def product_list(request):
     return JsonResponse(data)
 
 def product_create(request):
-    all_valid = True
-    count_product_errors = 0
-    products_report = []
-    products = []
-    for product in request.data.products:
-        product_form = forms.ProductForm(request.POST or None, instance=Product())
-        if product_form.is_valid():
-            product = product_form.save()
-            products.append(product)
-        else:
-            all_valid = False
-            count_product_errors += 1
-            errors = []
-            for key,value in reschedule_form.errors.items():
-                error_msg +="{}: {}".format(key, ','.join(value))
-                errors.append(error_msg)
-            products_report.append({
-                'product_id': str(product.id),
-                'errors': errors
+    if request.method == "POST":
+        all_valid = True
+        count_product_errors = 0
+        products_report = []
+        products = []
+        for product in request.POST.get('products'):
+            product_form = forms.ProductForm(request.POST or None, instance=Product())
+            if product_form.is_valid():
+                product = product_form.save()
+                products.append(product)
+            else:
+                all_valid = False
+                count_product_errors += 1
+                errors = []
+                for key,value in reschedule_form.errors.items():
+                    error_msg +="{}: {}".format(key, ','.join(value))
+                    errors.append(error_msg)
+                products_report.append({
+                    'product_id': str(product.id),
+                    'errors': errors
+                })
+        # Add products in a bulk creation
+        if not all_valid:
+            response = json.dumps({
+                'status': 'ERROR',
+                'products_report': products_report,
+                'number_of_products_unable_to_parse': count_product_errors
             })
-    # Add products in a bulk creation
-    if not all_valid:
+            return HttpResponse(response, status=422, content_type="application/json")
+        Product.objects.bulk_create(products)
         response = json.dumps({
-            'status': 'ERROR',
-            'products_report': products_report,
-            'number_of_products_unable_to_parse': count_product_errors
+            'status': 'OK'
         })
-        return HttpResponse(response, status=422, content_type="application/json")
-    Product.objects.bulk_create(products)
-    response = json.dumps({
-        'status': 'OK'
-    })
-    return HttpResponse(response, status=200, content_type="application/json")
+        return HttpResponse(response, status=200, content_type="application/json")
--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from .product import views as product_views
 
 urlpatterns = [
     url(r'^products/$', product_views.product_list),
-    url(r'^products/bulk_insert/$', product_views.product_list),
+    url(r'^products/bulk_insert/$', product_views.product_create),
 ]
